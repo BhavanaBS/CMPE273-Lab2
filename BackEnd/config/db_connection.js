595187e83b426ce76dc595b6c48dd11f80b91177
BhavanaBS <bhavana.bs1566@gmail.com>
2020-10-17 13:22:37 -0700

    Initial project set up
    Initail project set up with required files and login pages.

---
+++ BackEnd/config/db_connection.js
@@ -0,0 +1,5 @@
+const config = {
+  mongoDB: 'mongodb+mongodb+srv://BhavanaLocalhost:NauDi123@yelplab2.pvml1.mongodb.net/YelpLab2?retryWrites=true&w=majority://admin:AdminPassword@cluster0-sv2fg.mongodb.net/grubhub?retryWrites=true&w=majority'
+};
+
+module.exports = config;
